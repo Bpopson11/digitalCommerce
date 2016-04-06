c859b40b803bec30c92aaa86e63abc88ee1501de
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-04-06 11:26:22 -0700

    successfully set up thumbnails with pictures showing

--- app/routes/store.js
+++ app/routes/store.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('item');
+  },
   actions: {
      showModal: function(name, model) {
        this.render(name, {
