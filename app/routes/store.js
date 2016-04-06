1aeaa8873f032e90a607be9eae18d9e6c842333c
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-04-06 10:51:06 -0700

    begin set up on modal with 4 sample items

--- app/routes/store.js
+++ app/routes/store.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-});
+  actions: {
+     showModal: function(name, model) {
+       this.render(name, {
+         into: 'store',
+         outlet: 'modal',
+         model: model
+       });
+     },
+     removeModal: function() {
+       this.disconnectOutlet({
+         outlet: 'modal',
+         parentView: 'store'
+       });
+     }
+   }
+ });
