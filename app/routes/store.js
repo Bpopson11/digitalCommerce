14cc71df1b7333a2ea6085436ae77b6b43844a7f
Afton Downey and Bri Popson <bpopson@gmail.com>
2016-04-06 14:53:17 -0700

    scrap modal attempt and add basic page styling

--- app/routes/store.js
+++ app/routes/store.js
@@ -3,20 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('item');
-  },
-  actions: {
-     showModal: function(name, model) {
-       this.render(name, {
-         into: 'store',
-         outlet: 'modal',
-         model: model
-       });
-     },
-     removeModal: function() {
-       this.disconnectOutlet({
-         outlet: 'modal',
-         parentView: 'store'
-       });
-     }
-   }
- });
+  }
+
+});
