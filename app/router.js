fc03cf919c3cc27bb8f3d1a69b6276a83899e6cc
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-04-06 17:00:49 -0700

    add category models to items.json

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('admin');
   this.route('store');
   this.route('cart');
+  this.route('category', {path: '/category/:category_id'});
 });
 
 export default Router;
